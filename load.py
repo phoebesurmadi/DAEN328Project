9b79bc9dbf7398c5fe17ec98f25d4a40ac91e5cf
LuisHaddad04 <luishaddad04@tamu.edu>
2025-04-08 15:23:40 -0500

    Update load.py

--- load.py
+++ load.py
@@ -1,3 +1,8 @@
+load_dotenv()
 
-
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_NAME = os.getenv("POSTGRES_NAME")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 

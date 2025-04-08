fbedddc7499ac2620673ef9e22f30d2b4a6cea85
LuisHaddad04 <luishaddad04@tamu.edu>
2025-04-08 15:31:01 -0500

    Update load.py

--- load.py
+++ load.py
@@ -6,3 +6,22 @@ POSTGRES_NAME = os.getenv("POSTGRES_NAME")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
+conn = psycopg2.connect(
+  host = POSTGRES_HOST,
+  port = POSTGRES_PORT
+  dbname = POSTGRES_NAME
+  user = POSTGRES_USER
+  password = POSTGRES_PASSWORD
+)
+
+cur = conn.cursor()
+
+for index, row in df.iterrows():
+  cur.execute(
+    "INSERT INTO trends (keyword, date, trend_score) VALUES (%s, %s, %s)",
+    (row['keyword'], row['date'], row['trend_score'])
+  )
+
+conn.commit()
+cur.close()
+conn.close()

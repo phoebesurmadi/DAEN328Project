d95e774451303cef38103645a4e9a34f679ffda9
madelineshah4 <madelineshah4@gmail.com>
2025-04-15 15:05:44 -0500

    finished extract and transform, added data folder

--- extract.py
+++ extract.py
@@ -1,27 +1,21 @@
-from pytrends.request import TrendReq
+
+#       EXTRACTION
+
 import pandas as pd
-
-# Initialize Pytrends with a retry session
-pytrends = TrendReq(hl='en-US', tz=360)
-
-# test words
-kw_list = ["Excel", "Microsoft"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 12-m', geo='', gprop='')
-
-#test
-data = pytrends.interest_over_time()
+import requests
+import os
 
 
-data.head()
+# fetch data from the API endpoint with a limit of 1000 rows
+url = "https://data.cityofchicago.org/resource/4ijn-s7e5.csv?$limit=1000&$offset=0"
+response = requests.get(url)
 
+# load data into a pandas DataFrame from the response content
+data = pd.read_csv(url)
 
+# save
+output_path = 'DAEN328_Project/data/Messy_Data.csv'
 
-
-
-
-
-
-
-
-
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+data.to_csv(output_path, index=False)
 
--- transform.py
+++ transform.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import os
 
+# load messy data
+input_path = 'DAEN328_Project/data/Messy_Data.csv'
+data = pd.read_csv(input_path)
+
+
+#       CLEANING
+
+def clean(df):
+    df['inspection_date'] = pd.to_datetime(df['inspection_date']).dt.strftime('%Y-%m-%d')
+    df = df.drop(columns=['aka_name', 'license_', 'city', 'state', 'violations', 'location'])
+    df = df[df['results'] != 'No Entry']
+    df = df.dropna()
+    return df
+
+# clean
+data = clean(data)
+
+# save
+output_path = 'DAEN328_Project/data/Clean_Data.csv'
+
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+data.to_csv(output_path, index=False)
